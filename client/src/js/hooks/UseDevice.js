099240b83cc5d3a5d1d75779ca166d03774d4e59
Nico Kimmel <mail@nicokimmel.de>
2025-02-15 19:41:40 +0100

    Changed deviceType now only set on startup

--- client/src/js/hooks/UseDevice.js
+++ client/src/js/hooks/UseDevice.js
@@ -4,23 +4,14 @@ export default function useDevice() {
     const [deviceType, setDeviceType] = useState("desktop")
 
     useEffect(() => {
-        const handleResize = () => {
-            if (window.innerWidth < 435) {
-                setDeviceType("phone")
-            } else if (window.innerWidth < 700) {
-                setDeviceType("phablet")
-            } else if (window.innerWidth < 1140) {
-                setDeviceType("tablet")
-            } else {
-                setDeviceType("desktop")
-            }
-        }
-
-        handleResize()
-        window.addEventListener("resize", handleResize)
-
-        return () => {
-            window.removeEventListener("resize", handleResize)
+        if (window.innerWidth < 435) {
+            setDeviceType("phone")
+        } else if (window.innerWidth < 700) {
+            setDeviceType("phablet")
+        } else if (window.innerWidth < 1140) {
+            setDeviceType("tablet")
+        } else {
+            setDeviceType("desktop")
         }
     }, [])
 
